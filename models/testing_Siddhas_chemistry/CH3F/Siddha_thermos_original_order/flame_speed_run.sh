518629300861bfcaa7d05ed8e40c02e8930dbe63
Nora Khalil <khalil.nor@northeastern.edu>
2023-07-19 11:56:26 -0400

    siddhas with thermos in original order

---
+++ models/testing_Siddhas_chemistry/CH3F/Siddha_thermos_original_order/flame_speed_run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=fc_CH3F_Siddhas
+#SBATCH --error=fc.slurm.log
+#SBATCH --output=fc_output.slurm.log
+##SBATCH --cpus-per-task=5
+##SBATCH --mem-per-cpu=8Gb
+##SBATCH --ntasks=1 
+##SBATCH --array=1
+#SBATCH --partition=short 
+
+
+source activate cantera_env
+python flame_speed_calc.py

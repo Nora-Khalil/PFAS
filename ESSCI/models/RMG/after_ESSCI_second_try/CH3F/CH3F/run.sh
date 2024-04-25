49685a0fff68de6511863ac301838b1c70f2f9be
Nora Khalil <khalil.nor@northeastern.edu>
2024-04-25 09:27:51 -0400

    files

---
+++ ESSCI/models/RMG/after_ESSCI_second_try/CH3F/CH3F/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#SBATCH --nodes=1
+#SBATCH --time=25-00:00:00
+#SBATCH --job-name=CH3F
+#SBATCH --error=rmg.slurm.log
+#SBATCH --cpus-per-task=5
+#SBATCH --mem-per-cpu=8Gb
+#SBATCH --ntasks=1 
+#SBATCH --array=1
+#SBATCH --partition=west
+
+python-jl /home/khalil.nor/Code/RMG-Py/rmg.py input.py

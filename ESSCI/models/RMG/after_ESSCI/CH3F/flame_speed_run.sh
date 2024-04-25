e62f08fa4c2b692e0c7dd6b359c3ffa7d7bb808a
Nora Khalil <khalil.nor@northeastern.edu>
2024-04-25 09:17:42 -0400

    CH3F

---
+++ ESSCI/models/RMG/after_ESSCI/CH3F/flame_speed_run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=fc_CH3F
+#SBATCH --error=fc.slurm.log
+#SBATCH --output=fc_output.slurm.log
+##SBATCH --cpus-per-task=5
+##SBATCH --mem-per-cpu=8Gb
+##SBATCH --ntasks=1 
+##SBATCH --array=1
+#SBATCH --partition=short
+
+
+python flame_speed_calc.py

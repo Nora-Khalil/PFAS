631639a07e13805179db348186ca4f571bcabcde
Nora Khalil <khalil.nor@northeastern.edu>
2024-04-25 09:27:35 -0400

    files

---
+++ ESSCI/models/RMG/after_ESSCI_second_try/CH2F2/Fluorine_lib_up/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#SBATCH --nodes=1
+#SBATCH --time=25-00:00:00
+#SBATCH --job-name=dif
+#SBATCH --error=error.slurm.log
+#SBATCH --output=rmg.slurm.log
+#SBATCH --cpus-per-task=5
+#SBATCH --mem-per-cpu=8Gb
+#SBATCH --ntasks=1 
+#SBATCH --array=1
+#SBATCH --partition=west
+
+python-jl /home/khalil.nor/Code/RMG-Py/rmg.py input.py
+
+

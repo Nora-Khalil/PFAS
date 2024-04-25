3328cacc5c49916b1859fbfc04abf0e98002d8cf
Nora Khalil <khalil.nor@northeastern.edu>
2024-04-25 09:11:00 -0400

    4FP_CH2F2

---
+++ ESSCI/models/RMG/after_ESSCI/4FP_CH2F2/flame_speed_run-4.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=t_d_fluorine_lib
+#SBATCH --error=error_4.slurm.log
+#SBATCH --output=output_4.slurm.log
+##SBATCH --cpus-per-task=5
+##SBATCH --mem-per-cpu=8Gb
+##SBATCH --ntasks=1 
+#SBATCH --partition=short
+
+python flame_speed_calc_range_4.py
+
